3de5c6ac2ead0c8fc75c29e267feedb32703a61d
Timi <adetisola.timilehin@gmail.com>
2025-12-17 09:04:56 +0100

    Created a ScrollToTop component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
+import ScrollToTop from './components/ScrollToTop';
 import PostDesign from './pages/PostDesign';
 import DesignList from './pages/DesignList';
 import DesignDetail from './pages/DesignDetail';
@@ -7,6 +8,7 @@ import DesignDetail from './pages/DesignDetail';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen pt-24">
         <Navigation />
         <Routes>
